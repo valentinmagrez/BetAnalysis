1089b39306442d6b9507b4ba62cc53d00676b30f
valentinmagrez <valentin.magrez@gmail.com>
2020-02-16 21:44:03 +0100

    :sparkles: Add api call to check session to add cookie for futur api call

--- UnibetService/UnibetApi/UnibetClient.cs
+++ UnibetService/UnibetApi/UnibetClient.cs
@@ -1,29 +1,50 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
-using UnibetService.UnibetApi.DTO;
 
 namespace UnibetService.UnibetApi
 {
     public class UnibetClient
     {
-        private readonly HttpClient _client = new HttpClient();
+        private readonly HttpClient _client;
+        private readonly CookieContainer _cookieContainer;
+        private readonly Uri _baseUri = new Uri("https://www.unibet.fr");
 
-        private const string BaseUri = "https://www.unibet.fr/zones/";
-        
-        public async Task<LoginResponseDto> Login(string username, string password, string birthDate)
+        public UnibetClient()
         {
-            const string uriSuffix = "loginbox/processLogin.json";
+            _cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler {CookieContainer = _cookieContainer};
+            _client = new HttpClient(handler) {BaseAddress = _baseUri};
+        }
+        
+        public async Task Login(string username, string password, string birthDate)
+        {
+            const string uriSuffix = "/zones/loginbox/processLogin.json";
             var queryParams = $"username={username}&password={password}&dateOfBirth={birthDate}";
 
-            var fullUri = $"{BaseUri}{uriSuffix}?{queryParams}";
-            return await Post<LoginResponseDto>(fullUri);
+            var fullUri = $"{uriSuffix}?{queryParams}";
+            await Post(fullUri);
         }
 
-        private async Task<T> Post<T>(string uri)
+        public async Task CheckSession()
         {
-            var response = await _client.PostAsync(uri, null);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsAsync<T>();
+            const string uriSuffix = "/zones/checksession.json";
+            _cookieContainer.Add(_baseUri, new Cookie("CookieName", "cookie_value"));
+            await Post(uriSuffix);
+        }
+
+        private async Task Post(string uri)
+        {
+            await _client.PostAsync(uri, null);
+        }
+
+        private void DisplayCookie()
+        {
+            foreach (Cookie cookie in _cookieContainer.GetCookies(_baseUri))
+            {
+                Console.WriteLine(cookie.Name +": "+cookie.Value);
+            }
         }
     }
 }
