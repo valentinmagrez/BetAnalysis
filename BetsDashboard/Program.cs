e1d5cb7fc6aace3bcdb7042f482853517b9e47fd
valentinmagrez <valentin.magrez@gmail.com>
2020-02-22 16:55:00 +0100

    :sparkles: add chart support to blazor app

--- BetsDashboard/Program.cs
+++ BetsDashboard/Program.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 namespace BetsDashboard
 {
     public class Program
@@ -12,9 +10,20 @@ namespace BetsDashboard
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services
+                .AddBlazorise(options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                })
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
             builder.RootComponents.Add<App>("app");
+            var host = builder.Build();
 
-            await builder.Build().RunAsync();
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+            await host.RunAsync();
         }
     }
 }
