ad27a6a2f56d19414e5fa5bae49c38000ef6c10f
valentinmagrez <valentin.magrez@gmail.com>
2020-02-26 14:03:43 +0100

    :sparkles: extract unibet service to clean code and make tasks in //

---
+++ UnibetService/UnibetApi/UnibetService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnibetService.UnibetApi.DTO;
+
+namespace UnibetService.UnibetApi
+{
+    public class UnibetService
+    {
+        private readonly UnibetClient _unibetClient;
+
+        public UnibetService()
+        {
+            _unibetClient = new UnibetClient();
+        }
+
+        public async Task Login(string username, string password, string birthDate)
+        {
+            await _unibetClient.Login(username, password, birthDate);
+        }
+
+        public async Task<List<BetDto>> GetBetsHistoryParallel(DateTime from, DateTime to)
+        {
+            var tasks = new List<Task<List<BetDto>>>();
+            var bets = new List<BetDto>();
+            var currentDateFrom = from;
+            for (var currentDateTo = from.AddMonths(1); currentDateTo <= to; currentDateFrom = currentDateTo.AddDays(1), currentDateTo = currentDateTo.AddMonths(1))
+            {
+                tasks.Add(_unibetClient.GetBetsHistory(currentDateFrom, currentDateTo));
+            }
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                bets.AddRange(task.Result);
+            }
+
+            return bets;
+        }
+    }
+}
