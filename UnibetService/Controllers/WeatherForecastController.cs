0b8d9d2c58bb29bb0d243b10530673361acd1054
valentinmagrez <valentin.magrez@gmail.com>
2020-02-27 22:51:27 +0100

    :lipstick: Add datepicker support

--- UnibetService/Controllers/WeatherForecastController.cs
+++ UnibetService/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using UnibetService.UnibetApi;
 
 namespace UnibetService.Controllers
 {
@@ -24,8 +25,13 @@ namespace UnibetService.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
+            var service = new UnibetApi.UnibetService();
+            await service.Login("krikicbojan", "krikicbojan2009", "23/05/1993");
+            var result1 = await service.GetAllBets();
+            var stake = result1.Where(_=>!_.IsFreeBet).Sum(_ => _.Stake);
+            var totalreturn = result1.Sum(_ => _.TotalReturn);
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
