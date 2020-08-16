ef83833c57e23a42413d19475ae51d1a7eeac0f7
valentinmagrez <valentin.magrez@gmail.com>
2020-08-16 22:45:56 +0200

    :sparkles: :lipstick: front app request BO to retrieve bets and display barchart

---
+++ BetsDashboard/Models/BetDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnibetService.Utils.Converters;
+
+namespace BetsDashboard.Models
+{
+    public class BetDto
+    {
+
+        [JsonProperty("date")]
+        [JsonConverter(typeof(MicrosecondEpochConverter))]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("stake")]
+        public decimal Stake { get; set; }
+
+        [JsonProperty("totalOdd")]
+        public double? Odd { get; set; }
+
+        [JsonProperty("totalReturn")]
+        public decimal? TotalReturn { get; set; }
+    }
+}
--- BetsDashboard/Program.cs
+++ BetsDashboard/Program.cs
@@ -1,8 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace BetsDashboard
 {
     public class Program
@@ -11,12 +15,13 @@ namespace BetsDashboard
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.Services
-                .AddBlazorise(options =>
-                {
-                    options.ChangeTextOnKeyPress = true;
-                })
+                .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
                 .AddBootstrapProviders()
-                .AddFontAwesomeIcons();
+                .AddFontAwesomeIcons()
+                .AddScoped(_ => new HttpClient
+                {
+                    BaseAddress = new Uri("https://localhost:32768")
+                });
             builder.RootComponents.Add<App>("app");
             var host = builder.Build();
 
